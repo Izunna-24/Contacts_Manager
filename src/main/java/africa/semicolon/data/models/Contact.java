2ed20c97f30ed9099e55ee33e38f3611a02e73d4
Izunajune <izuchukwuijeudo@gmail.com>
2024-04-04 03:29:21 +0100

    java

--- src/main/java/africa/semicolon/data/models/Contact.java
+++ src/main/java/africa/semicolon/data/models/Contact.java
@@ -2,7 +2,10 @@ package africa.semicolon.data.models;
 
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
 
 @Data
 @Document("contacts")
@@ -12,4 +15,7 @@ public class Contact {
     private String phoneNumber;
     private String email;
     private String username;
+
+    //@DBRef
+    //private List<Contact> contactList;
 }
