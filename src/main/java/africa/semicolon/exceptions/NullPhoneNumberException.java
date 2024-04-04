5dce213ac7b4432a3e9df85334c3a836d308993e
Izunajune <izuchukwuijeudo@gmail.com>
2024-04-04 02:07:21 +0100

    java

---
+++ src/main/java/africa/semicolon/exceptions/NullPhoneNumberException.java
@@ -0,0 +1,7 @@
+package africa.semicolon.exceptions;
+
+public class NullPhoneNumberException extends ContactManagerExceptions{
+    public NullPhoneNumberException(String message){
+        super(message);
+    }
+}
