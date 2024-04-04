1741a6e4a072e2b4b4b09c1d3ac62633bf6692e4
Izunajune <izuchukwuijeudo@gmail.com>
2024-04-04 03:30:41 +0100

    java

--- src/main/java/africa/semicolon/dataTransferObjects/EditContactRequest.java
+++ src/main/java/africa/semicolon/dataTransferObjects/EditContactRequest.java
@@ -6,7 +6,9 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Data
 @Document
 public class EditContactRequest {
+    //private String id;
     private String PhoneNumber;
     private String email;
+    private String username;
 
 }
