d1cb0590960d776d311022105131f66d8eca5807
Izunajune <izuchukwuijeudo@gmail.com>
2024-04-04 03:29:35 +0100

    java

---
+++ src/main/java/africa/semicolon/exceptions/ContactNotFoundException.java
@@ -0,0 +1,7 @@
+package africa.semicolon.exceptions;
+
+public class ContactNotFoundException extends ContactManagerExceptions{
+    public ContactNotFoundException(String message){
+        super(message);
+    }
+}
