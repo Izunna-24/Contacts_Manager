ab6cadf10655d1b806e969ba8a07e05ff6a324e7
Izunajune <izuchukwuijeudo@gmail.com>
2024-04-04 02:07:04 +0100

    java

---
+++ src/main/java/africa/semicolon/exceptions/NullEmailException.java
@@ -0,0 +1,7 @@
+package africa.semicolon.exceptions;
+
+public class NullEmailException extends ContactManagerExceptions{
+    public NullEmailException(String message){
+        super(message);
+    }
+}
