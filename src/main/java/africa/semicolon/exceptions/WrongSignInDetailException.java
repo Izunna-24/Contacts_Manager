59de2100f8f22a77dfd61fc83cc1b80aefdc77c3
Izunajune <izuchukwuijeudo@gmail.com>
2024-04-06 00:49:33 +0100

    java

---
+++ src/main/java/africa/semicolon/exceptions/WrongSignInDetailException.java
@@ -0,0 +1,7 @@
+package africa.semicolon.exceptions;
+
+public class WrongSignInDetailException extends ContactManagerExceptions{
+    public WrongSignInDetailException(String message){
+        super(message);
+    }
+}
