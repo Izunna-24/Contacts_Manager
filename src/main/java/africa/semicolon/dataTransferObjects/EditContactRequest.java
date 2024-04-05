f59fba9fd600bee9aeec9a419cf7e75b42ee1623
Izunajune <izuchukwuijeudo@gmail.com>
2024-04-06 00:46:24 +0100

    java

--- src/main/java/africa/semicolon/dataTransferObjects/EditContactRequest.java
+++ src/main/java/africa/semicolon/dataTransferObjects/EditContactRequest.java
@@ -1,12 +1,14 @@
 package africa.semicolon.dataTransferObjects;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Document
 public class EditContactRequest {
-    //private String id;
+    @Id
+    private String id;
     private String PhoneNumber;
     private String email;
     private String username;
