27d6e6551ae6ce212610d8159a6a3dcf14ccb36f
Izunajune <izuchukwuijeudo@gmail.com>
2024-04-06 00:48:24 +0100

    java

--- src/main/java/africa/semicolon/services/UserServices.java
+++ src/main/java/africa/semicolon/services/UserServices.java
@@ -4,12 +4,11 @@ import africa.semicolon.data.models.Contact;
 import africa.semicolon.dataTransferObjects.*;
 
 public interface UserServices {
-void signUp(SignUpRequest signUpRequest);
+void signUpWith(SignUpRequest signUpRequest);
 void signIn(SignInRequest signInRequest);
 void createContact(CreateContactRequest createContactRequest);
 void deleteContact(DeleteContactRequest deleteContactRequest);
 void editContact(EditContactRequest editContactRequest);
-Contact findContactByEmail(String email);
-Contact findContactByPhoneNumber(String  phoneNumber);
-Contact findContactByUsername(String username);
+Contact findContactById(String id);
+
 }
