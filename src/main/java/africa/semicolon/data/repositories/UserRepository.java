17f8b1a2d8ba8705fde67c2a2cdcc324a142ba40
Izunajune <izuchukwuijeudo@gmail.com>
2024-04-06 00:47:49 +0100

    java

--- src/main/java/africa/semicolon/data/repositories/UserRepository.java
+++ src/main/java/africa/semicolon/data/repositories/UserRepository.java
@@ -4,9 +4,10 @@ import africa.semicolon.data.models.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
-    Optional<User> findUserByUsername(String username);
-    Optional<User> findUserById(String id);
+    User findUserByUsername(String username);
+    User findUserById(String id);
+    User findUserByEmail(String email);
+
 }
