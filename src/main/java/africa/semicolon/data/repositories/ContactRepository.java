a9e9b34a70696b44d0389a9f65bd95b365bec34c
Izunajune <izuchukwuijeudo@gmail.com>
2024-04-06 00:44:10 +0100

    java

--- src/main/java/africa/semicolon/data/repositories/ContactRepository.java
+++ src/main/java/africa/semicolon/data/repositories/ContactRepository.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 public interface ContactRepository extends MongoRepository<Contact, String> {
     Optional<Contact> findContactById(String id);
     Optional<Contact> findContactByPhoneNumber(String phoneNumber);
-    Optional<Contact> findContactByUsername(String username);
+
 
     Optional<Contact> findContactByEmail(String email);
 
